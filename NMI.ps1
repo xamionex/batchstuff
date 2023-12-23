aa23cd646688120726ddacb16b73b0ace43d7ac9
amione <57235791+xamionex@users.noreply.github.com>
2023-12-23 11:52:41 +0100

    bad code for making all minecraft instances linked
    to be used with prismlauncher and then running as `NMI.ps1 "path\to\instance\"` example: `NMI.ps1 "C:\Users\Petar\AppData\Roaming\PrismLauncher\instances\Versatile"`

---
+++ NMI.ps1
@@ -0,0 +1,69 @@
+#at top of script
+if (!
+    #current role
+    (New-Object Security.Principal.WindowsPrincipal(
+        [Security.Principal.WindowsIdentity]::GetCurrent()
+        #is admin?
+    )).IsInRole(
+        [Security.Principal.WindowsBuiltInRole]::Administrator
+    )
+) {
+    #elevate script and exit current non-elevated runtime
+    Start-Process `
+        -FilePath 'powershell' `
+        -ArgumentList (
+        #flatten to single array
+        '-File', $MyInvocation.MyCommand.Source, $args `
+        | % { $_ }
+    ) `
+        -Verb RunAs
+    exit
+}
+
+$selection = "$args"
+if (Test-Path -LiteralPath "${selection}\.minecraft") { Set-Location -LiteralPath "${selection}\.minecraft" }
+elseif (Test-Path -LiteralPath "${selection}\minecraft") { Set-Location -LiteralPath "${selection}\minecraft" }
+else {
+    Write-Output ".minecraft doesnt exist inside instance"
+    exit
+}
+${curdir} = Get-Location
+${appdatamc} = "${env:APPDATA}\.minecraft"
+${folders} = "screenshots", "resourcepacks", "saves", "schematics", "shaderpacks", "texturepacks"
+
+Write-Output "Created symlinks for:"
+foreach (${folder} in ${folders}) {
+    $instancefolder = "${curdir}\${folder}"
+    $appdatafolder = "${appdatamc}\${folder}"
+
+    # Create the folders if they don't exist
+    if (!(Test-Path -LiteralPath ${instancefolder})) {
+        New-Item -ItemType Directory "${instancefolder}"
+    }
+    if (!(Test-Path -LiteralPath ${appdatafolder})) {
+        New-Item -ItemType Directory "${appdatafolder}"
+    }
+
+    # Copy existing files to appdata folder and remove the instance folder, then create a symlink
+    Copy-Item "${instancefolder}\*" -Destination "${appdatafolder}\" -Recurse
+    Remove-Item "${instancefolder}" -Recurse -Force
+    New-Item -ItemType SymbolicLink "${folder}" -Target "${appdatafolder}" -Force
+}
+
+# Create if doesn't exist
+if (!(Test-Path -LiteralPath "servers.dat")) {
+    New-Item -ItemType File "servers.dat"
+}
+if (!(Test-Path -LiteralPath "${appdatamc}\servers.dat")) {
+    New-Item -ItemType File "${appdatamc}\servers.dat"
+}
+
+# Rename servers.dat to old_currdate
+$DateStr = Get-Date -Format "yyyy-MM-dd-hh-mm-ss"
+Rename-Item "servers.dat" "servers.dat_old.${DateStr}"
+
+# Link servers.dat to appdata one
+if (!(Test-Path -LiteralPath "servers.dat")) {
+    New-Item -ItemType SymbolicLink "servers.dat" -Target "${appdatamc}\servers.dat"
+}
+pause
